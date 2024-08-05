82938ba6ea04b5eb954db84426ea5be8facf663b
Girish <girish@algotechnologies.in>
2024-08-05 22:54:21 +0530

    update base url

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const BASE_URL =
-  'https://isqhnlnlxj.execute-api.ap-south-1.amazonaws.com/development';
+  "https://isqhnlnlxj.execute-api.ap-south-1.amazonaws.com/v1";
 export const WEB_SOCKET_URL =
-  'wss://s85rlq0udb.execute-api.ap-south-1.amazonaws.com/development';
+  "wss://s85rlq0udb.execute-api.ap-south-1.amazonaws.com/v1";
